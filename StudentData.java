c42ac879004012ec7004ff66dc93dbd2ed1e0a67
EbrahimshaGudhus <151456533+EbrahimshaGudhus@users.noreply.github.com>
2023-11-21 13:19:19 +0530

    Add files via upload
    week1.day1 - Assignment
    Create Class Student declare variables "studentName ,rollNo ,collegeName, markScored and cgpa"

---
+++ StudentData.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class StudentData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String studentName= "Shane";
+		int rollNo =27;
+		String collegeName = "SRR Engineering Collge";
+		int score = 85;
+		String cgpa = "8.5%";
+		
+System.out.println("The Student name " + studentName);
+System.out.println("Roll Number " + rollNo);
+System.out.println("College Name "+ collegeName);
+System.out.println("Mark Scored " + score);
+System.out.println("The Percentage "+ cgpa);
+		
+
+	}
+
+}
